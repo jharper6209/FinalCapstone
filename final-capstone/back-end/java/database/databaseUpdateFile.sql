9ddd8f992a770b86dcfd54d8bb12e794c419ff9a
Nia Tucker <ntucker1@kent.edu>
2021-08-04 16:06:45 -0400

    update

--- final-capstone/back-end/java/database/databaseUpdateFile.sql
+++ final-capstone/back-end/java/database/databaseUpdateFile.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS ingredient CASCADE;
 DROP TABLE IF EXISTS category CASCADE;
 DROP TABLE IF EXISTS recipe CASCADE;
-?
+
 CREATE TABLE recipe (
 recipe_id SERIAL NOT NULL,
 category_id INTEGER NOT NULL,
 recipe_name CHARACTER VARYING(50) NOT NULL,
 image CHARACTER VARYING(100) NOT NULL,
-user_id INTEGER NOT NULL,
+
 CONSTRAINT pk_recipe_recipe_id PRIMARY KEY (recipe_id)
 );
 
@@ -17,13 +17,13 @@ ingredient_name CHARACTER VARYING(50) NOT NULL,
 recipe_id INTEGER NOT NULL,
 CONSTRAINT pk_ingredient_id PRIMARY KEY (ingredient_id)
 );
-?
+
 CREATE TABLE category(
 category_id SERIAL NOT NULL,
 category_name CHARACTER VARYING(50) NOT NULL,
 CONSTRAINT pk_category_category_id PRIMARY KEY (category_id)
 );
-?
+
 ALTER TABLE recipe ADD CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES category(category_id);
 ALTER TABLE ingredient ADD CONSTRAINT fk_recipe_recipe_id FOREIGN KEY (recipe_id) REFERENCES recipe(recipe_id);
 ALTER TABLE recipe ADD CONSTRAINT fk_users_user_id FOREIGN KEY (user_id) REFERENCES users(user_id);
@@ -35,16 +35,16 @@ VALUES
 ('Chicken'), 
 ('Pasta'), 
 ('Vegan');
-?
-?
-?
+
+
+
 INSERT INTO recipe (category_id, recipe_name, image)
 VALUES(1, 'Beef and Mustard Pie', 'https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg'),
 (2, 'Ayam Percik','https://www.themealdb.com/images/media/meals/020z181619788503.jpg'),
 (3, 'Fettucine alfredo', 'https://www.themealdb.com/images/media/meals/uquqtu1511178042.jpg'),  
 (4, 'Vegan Chocolate Cake', 'https://www.themealdb.com/images/media/meals/qxutws1486978099.jpg');
-?
-	BEEF AND MUSTARD PIE INGREDIENTS
+
+	
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Butter' , 1);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Beef Stock', 1);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Beef', 1);
@@ -62,7 +62,7 @@ INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Salt', 1);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Onion', 1);
 
 
-	AYAM PERCIK INGREDIENTS
+	
 INSERT INTO ingredient (ingredient_name , recipe_id) VALUES ('Chicken Thighs', 2);
 INSERT INTO ingredient (ingredient_name , recipe_id) VALUES ('Challots' , 2);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Ginger', 2);
@@ -77,7 +77,7 @@ INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Coconut Milk', 2);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Sugar', 2);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Water', 2);
 
-	VEGAN CHOCOLATE CAKE
+	
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Self Rising Flour', 3);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Coco Sugar', 3);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Cacao', 3);
@@ -87,7 +87,7 @@ INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Almond Milk', 3);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Vanilla', 3);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Water', 3);
 
-	FETTUCINE ALFREDO
+
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Butter' , 4);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Corn Flour', 4);
 INSERT INTO ingredient (ingredient_name, recipe_id) VALUES ('Fettucine', 4);
