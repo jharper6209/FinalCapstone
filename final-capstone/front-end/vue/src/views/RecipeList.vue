53c5713f65492876c55e805c43d785b82eb22bb1
Jessica Harper <j.harper6209@gmail.com[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C>
2021-08-05 16:47:16 -0400

    Added checkboxs and Recipe titles

--- final-capstone/front-end/vue/src/views/RecipeList.vue
+++ final-capstone/front-end/vue/src/views/RecipeList.vue
@@ -1,20 +1,19 @@
 <template>
-  <div>
+
   
 
     <recipe-list/>
-  </div>
+
 </template>
 
 <script>
 import RecipeList from '../components/RecipeList.vue'
 export default {
-    components:{
-        RecipeList
-    }
+    components:{RecipeList }
 }
 </script>
 
 <style>
 
 </style>
+
